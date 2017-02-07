2909171b9a9f591e53eda3835d13dc94be1d59f7
luluco250 <luluco250@gmail.com>
2017-02-07 20:55:00 -0200

    Major changes. Get's use generics now.

--- INIReader.cs
+++ INIReader.cs
@@ -1,19 +1,26 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
 public class INIReader {
-    private Dictionary<string, Dictionary<string, string>> dictionary = new Dictionary<string, Dictionary<string, string>>();
+    /// Dictionary used to store the values parsed from the ini file.
+    public Dictionary<string, Dictionary<string, string>> dictionary = new Dictionary<string, Dictionary<string, string>>();
 
-    public INIReader(string filename) {
+    /// <summary>
+    /// Parse an INI file to a dictionary.
+    /// <para><param name="filePath">Path to the INI file.</param></para>
+    /// </summary>
+    public INIReader(string filePath) {
         StringBuilder sb = new StringBuilder("");
-        string name = "";
-        string value = "";
-        string category = "";
+        string category = "", name = "", value = "";
         dictionary.Add("", new Dictionary<string, string>());
-        string[] lines = File.ReadAllLines(filename);
+        string[] lines = File.ReadAllLines(filePath);
+
         for (int l = 0; l < lines.Length; ++l) {
+            sb.Length = 0; //reset sb in case a parsing error in the previous line left it with garbage data
             lines[l].TrimStart(' ', '\t'); //trim any whitespace
+
             if (lines[l] == "" || lines[l][0] == ';' || lines[l][0] == '#') {
                 continue; //skip line if it's empty and/or is a comment
             }
@@ -23,20 +30,19 @@ public class INIReader {
                 }
                 category = sb.ToString();
                 dictionary.Add(category, new Dictionary<string, string>());
-                sb.Length = 0;
                 continue;
             }
+
             for (int c = 0; c < lines[l].Length; ++c) {
                 //each character of the current line
                 if (lines[l][c] == '=') {
                     name = sb.ToString();
-                    sb.Length = 0;
+                    sb.Length = 0; //reset sb for building the value
                     ++c;
                 }
                 sb.Append(lines[l][c]);
                 if (c == (lines[l].Length - 1)) {
                     value = sb.ToString();
-                    sb.Length = 0;
                     break;
                 }
             }
@@ -45,56 +51,49 @@ public class INIReader {
         }
     }
 
-    public string GetString(string category, string name, string defaultValue) {
+    /// <summary>
+    /// Get the value of Type type from the INI file.
+    /// <para><param name="category">The [category] in which the value is located in the INI file.</param></para>
+    /// <para><param name="name">Name of the value.</param></para>
+    /// <para><param name="defaultValue">Default value in case the value is not found or there's a type mismatch.</param></para>
+    /// </summary>
+    public type Get<type>(string category, string name, type defaultValue) {
         try {
-            return dictionary[category][name];
+            return (type)Convert.ChangeType(dictionary[category][name], typeof(type));
         }
         catch {
             return defaultValue;
         }
     }
 
-    public string GetString(string category, string name) {
-        return GetString(category, name, "");
+    /// <summary>
+    /// Get the value of Type type from the INI file.
+    /// <para><param name="category">The [category] in which the value is located in the INI file.</param></para>
+    /// <para><param name="name">Name of the value.</param></para>
+    /// <para><para>'default(type)' is used in case the value is not found or there's a type mismatch.</para></para>
+    /// </summary>
+    public type Get<type>(string category, string name) {
+        return Get<type>(category, name, default(type));
     }
 
-    public bool GetBool(string category, string name, bool defaultValue) {
-        try {
-            return bool.Parse(GetString(category, name, ""));
-        } 
-        catch {
-            return defaultValue;
-        }
+    /// <summary>
+    /// Get the value of Type type from the INI file.
+    /// <para>Looks for values without a category.</para>
+    /// <para><param name="name">Name of the value.</param></para>
+    /// <para><para>'default(type)' is used in case the value is not found or there's a type mismatch.</para></para>
+    /// </summary>
+    public type Get<type>(string name) {
+        return Get<type>("", name, default(type));
     }
 
-    public bool GetBool(string category, string name) {
-        return GetBool(category, name, false);
-    }
-
-    public int GetInt(string category, string name, int defaultValue) {
-        try {
-            return int.Parse(GetString(category, name, ""));
-        }
-        catch {
-            return defaultValue;
-        }
-    }
-
-    public int GetInt(string category, string name) {
-        return GetInt(category, name, 0);
-    }
-
-    public float GetFloat(string category, string name, float defaultValue) {
-        try {
-            return float.Parse(GetString(category, name, ""));
-        }
-        catch {
-            return defaultValue;
-        }
-    }
-
-    public float GetFloat(string category, string name) {
-        return GetFloat(category, name, 0.0f);
+    /// <summary>
+    /// Get the value of Type type from the INI file.
+    /// <para>Looks for values without a category.</para>
+    /// <para><param name="name">Name of the value.</param></para>
+    /// <para><param name="defaultValue">Default value in case the value is not found or there's a type mismatch.</param></para>
+    /// </summary>
+    public type Get<type>(string name, type defaultValue) {
+        return Get<type>("", name, defaultValue);
     }
 
 }
